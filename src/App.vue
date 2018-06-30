2d1a997766e5eba9a6d722c8ef4f9142982c5a05
Olivier <olivier.roger@gmail.com>
2018-06-30 14:23:33 +0200

    Replace App CSS

--- src/App.vue
+++ src/App.vue
@@ -22,7 +22,7 @@ export default {
 </script>
 
 <style>
-  html,
+/**  html,
   body {
     font-size: 14px;
     line-height: 1.5;
@@ -95,7 +95,6 @@ export default {
 
   .inbox-messages {
     margin-top: 0px;
-    /* was 60px */
   }
 
   .inbox-messages a {
@@ -151,7 +150,6 @@ export default {
 
   .message-preview {
     margin-top: 0px;
-    /* was 60px */
   }
 
   .message-preview .avatar {
@@ -179,8 +177,6 @@ export default {
     background-image: none;
   }
 
-  /* ---- Hover ---- */
-
   button:hover {
     opacity:0.7;
   }
@@ -193,10 +189,264 @@ export default {
 
   a.action.ripple {
     color: white;
+  } */
+
+  html,
+  body {
+    height: 100%;
   }
 
-  /**
-  Steemit specific formatting classes
+  body {
+    width: 100%;
+    overflow-x: hidden;
+  }
+
+  a,
+  .btn-link,
+  .page-link {
+    color: #17a2b8;
+  }
+
+  a:hover,
+  .btn-link:hover,
+  .page-link:hover,
+  a:focus,
+  .btn-link:focus,
+  .page-link:focus {
+    color: #138496;
+    text-decoration: none;
+  }
+
+  .btn,
+  .btn-sm,
+  .btn-lg,
+  .form-control,
+  .page-item:first-child .page-link,
+  .page-item:last-child .page-link {
+    border-radius: 0;
+  }
+
+  .modal-title {
+    font-size: 1rem;
+  }
+
+  .modal-content {
+    border-radius: 0;
+  }
+
+  .modal-body .profile-image {
+    width: 56px;
+    height: 56px;
+    background-position: center center;
+    background-size: cover;
+  }
+
+  .modal-body .user {
+    line-height: 16px;
+  }
+
+  .modal-body .user .profile-link {
+    text-decoration: none;
+  }
+
+  /* NAVBAR */
+  .navbar-dark .navbar-nav .nav-link {
+    position: relative;
+    color: rgba(255, 255, 255, .9);
+  }
+
+  .navbar-dark .navbar-text {
+    color: rgba(255, 255, 255, .9);
+  }
+
+  .badge-overlap {
+    position: absolute;
+    top: 0;
+    right: 0;
+    font-size: .7rem;
+    padding: 3px 6px;
+  }
+
+  .main-menuitem:after {
+    display: none;
+  }
+
+  /* CONTENT */
+
+  #content {
+    width: 200%;
+    height: 100%;
+    background: #eee;
+    padding: 56px 0;
+    transition: margin-left .3s ease;
+  }
+
+  #content.show-left {
+    margin-left: 0;
+  }
+
+  #content.show-right {
+    margin-left: -100%;
+  }
+
+  #list,
+  #reply {
+    width: 50%;
+    height: 100%;
+    float: left;
+    overflow-y: auto;
+  }
+
+  #list .list-group-item {
+    padding: 0;
+  }
+
+  #list .reply {
+    position: relative;
+  }
+
+  #list .reply:focus {
+    box-shadow: none;
+  }
+
+  #list .user {
+    line-height: 16px;
+  }
+
+  #list .user .profile-link {
+    text-decoration: none;
+  }
+
+  #list .comment-profile-image {
+    width: 38px;
+    height: 38px;
+    background-position: center center;
+    background-size: cover;
+    float: left;
+    margin-right: 5px;
+  }
+
+  #list .comment-footer .external-links {
+    line-height: 38px;
+  }
+
+  #reply {
+    background: #fff;
+  }
+
+  #reply .user {
+    line-height: 16px;
+  }
+
+  #reply .user .profile-link {
+    text-decoration: none;
+  }
+
+  #reply .comment-profile-image {
+    width: 38px;
+    height: 38px;
+    background-position: center center;
+    background-size: cover;
+    float: left;
+    margin-right: 5px;
+  }
+
+  .vote-weight {
+    position: relative;
+  }
+
+  .vote-weight input {
+    width: 80px;
+    font-size: 1.2rem;
+  }
+
+  .vote-weight:after {
+    content: '%';
+    line-height: 42px;
+    position: absolute;
+    top: 0;
+    right: 10px;
+    color: #999;
+  }
+
+  .vote-controls {
+    margin-top: 5px;
+    padding: 4px 8px;
+  }
+
+  #reply .reply-form,
+  #reply .vote-form {
+    background: #fafafa;
+  }
+
+  @media (min-width: 768px) {
+    #content {
+      width: 100%;
+    }
+
+    #content.show-right {
+      margin-left: 0;
+    }
+
+    #list {
+      width: 50%;
+    }
+
+    #reply {
+      width: 50%;
+    }
+  }
+
+  @media (min-width: 768px) and (max-width: 1199px) {
+    #reply {
+      border-left: solid 1px #eee;
+    }
+  }
+
+  @media (min-width: 992px) {
+    #list {
+      width: 40%;
+    }
+
+    #reply {
+      width: 60%;
+    }
+  }
+
+  @media (min-width: 1199px) {
+    #list .list-group-item {
+      margin: 10px 10px 0;
+      border: none;
+    }
+
+    #list .list-group-item:last-child {
+      margin-bottom: 10px;
+    }
+
+    #reply .user {
+      padding: 10px 1rem;
+    }
+  }
+
+  @media (max-width: 575px) {
+    .tooltip {
+      display: none;
+    }
+
+    .nav-item.dropdown {
+      position: static;
+    }
+
+    .nav-item .dropdown-menu {
+      width: 100%;
+      border: 0;
+      border-radius: 0;
+      margin-top: 0;
+    }
+  }
+
+
+  /**  Steemit specific formatting classes
   https://steemit.com/steemit/@steemitblog/new-advanced-formatting-features
   **/
   .pull-left {
